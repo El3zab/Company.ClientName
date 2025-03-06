1d5a9792fc5ffc97554aec22f069a72d12846371
El3zab <ahmed.3zab99@gmail.com>
2025-03-07 00:38:06 +0200

    Add project files.

---
+++ Company.ClientName.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Company.ClientName.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
