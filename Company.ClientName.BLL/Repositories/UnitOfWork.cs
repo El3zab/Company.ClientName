7e388dd49f5b6d50503159da71dda36b9d27a25a
El3zab <ahmed.3zab99@gmail.com>
2025-03-19 23:04:49 +0200

    Implement UnitOfWork Service in [Employee - Department]

---
+++ Company.ClientName.BLL/Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Company.ClientName.BLL.Interfaces;
+using Company.ClientName.DAL.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.ClientName.BLL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CompanyDbContext _context;
+
+        public IDepartmentRepository DepartmentRepository { get; } // NULL
+
+        public IEmployeeRepository EmployeeRepository { get; } // NULL
+
+        public UnitOfWork(CompanyDbContext context)
+        {
+            _context = context;
+            DepartmentRepository = new DepartmentRepository(_context);
+            EmployeeRepository = new EmployeeRepository(_context);
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
